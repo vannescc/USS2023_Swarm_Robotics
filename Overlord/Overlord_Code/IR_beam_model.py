47d22d0067539fc333f88ab844a95410f2d305c3
vannescc <120139813+vannescc@users.noreply.github.com>
2023-07-14 11:07:52 -0900

    Update IR_beam_model.py

--- Overlord/Overlord_Code/IR_beam_model.py
+++ Overlord/Overlord_Code/IR_beam_model.py
@@ -4,6 +4,9 @@ import socket
 import csv
 import statistics as stat
 import math
+from scipy.stats import norm
+
+# def prob(mean, sigma)
 
 file = open('IR_data_7_6.csv', 'r')
 reader = csv.reader(file)
@@ -14,7 +17,7 @@ numRows = len(listReader)
 readDist = []
 actualDist = []
 
-desired_dist = 30
+desired_dist = 6
 
 
 actDists = []
@@ -52,12 +55,93 @@ for i in range(len(readDists)):
     tempvar += ((readDists[i] - mean)**2)
 sigma = math.sqrt(tempvar/len(readDists))
 
-s = numpy.random.normal(mean, sigma, 1000)
+zStar = stat.mean(readDists)
 
-count, bins2, ignored = plt.hist(s, 50, density=True)
+phit = norm.pdf(bins, zStar, sigma)
+# print(phit[6]*len(readDists))
+
+plong = []
+for i in range(51):
+    if(i != 50):
+        plong.append(0)
+    else:
+        plong.append(1)
+
+# print(phit)
+tempval = 0
+for i in range(51):
+#     eta = 1/(phit[i])
+#     eihit = eta * phit[i]
+#     if(phit[i] == 0):
+    if(False):
+        eihit = 0
+    else:
+#         eta = 1/(phit[i] + float(plong[i]))
+#         eihit = eta * phit[i]
+        eihit = phit[i]/(phit[i] + float(plong[i]) + .001)
+#     print(eihit)
+#     print(phit)
+    tempVal += eihit
+whit = tempVal/51
+for j in range(1):
+    sum_eihit = 0
+    for i in range(51):
+#         eta = 1/(phit[i])
+#         eihit = eta * phit[i]
+#         if(phit[i] == 0):
+        if(False):
+            eihit = 0
+        else:
+#             eta = 1/(phit[i] + float(plong[i]))
+#             eihit = eta * phit[i]
+            eihit = phit[i]/(phit[i] + float(plong[i]) + .001)
+        sum_eihit += eihit
+    sum_eihit2 = 0
+    for i in range(51):
+#         eta = 1/(phit[i])
+#         eihit = eta * phit[i]
+#         if(phit[i] == 0):
+        if(False):
+            eihit = 0
+        else:
+#             eta = 1/(phit[i] + float(plong[i]))
+#             eihit = eta * phit[i]
+            eihit = phit[i]/(phit[i] + float(plong[i]) + .001)
+        sum_eihit2 += eihit * (i -zStar)**2
+#     print(sum_eihit)
+    sigma = numpy.sqrt((1/sum_eihit) * sum_eihit2)
+    phit = norm.pdf(bins, zStar, sigma)
+    tempval = 0
+    for i in range(51):
+#         eta = 1/(phit[i])
+#         eihit = eta * phit[i]
+#         if(phit[i] == 0):
+        if(False):
+            eihit = 0
+        else:
+#             eta = 1/(phit[i] + float(plong[i]))
+#             eihit = eta * phit[i]
+            eihit = phit[i]/(phit[i] + float(plong[i]) + .001)
+        print(eihit)
+        tempVal += eihit
+    whit = tempVal/51
+
+
+# s = numpy.random.normal(mean, sigma, 1000)
+
+# count, bins2, ignored = plt.hist(s, 50, density=True)
+
+        
+
+    
+        
 fig, ax = plt.subplots()
 ax.bar(bins, heights, width=1, edgecolor="white", linewidth=0.7)
-plt.plot(bins2, 1/(sigma * numpy.sqrt(2 * numpy.pi)) * numpy.exp( - (bins2 - mean)**2 / (2 * sigma**2) ) * len(readDists), linewidth=2, color='r')
+# plt.plot(bins, (norm.pdf(bins, mean, sigma) + plong) * len(readDists))
+# plt.plot(bins, (norm.pdf(bins, zStar, sigma) + plong) * len(readDists))
+plt.plot(bins, (norm.pdf(bins, zStar, sigma)) * len(readDists)) #whit
+print(sigma)
+# plt.plot(bins2, 1/(sigma * numpy.sqrt(2 * numpy.pi)) * numpy.exp( - (bins2 - mean)**2 / (2 * sigma**2) ) * len(readDists), linewidth=2, color='r')
 plt.show()
 
 
